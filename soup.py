00e6fae4349f75fe323a7766eedf3931ad58d1ce
elichad <eli.chadwick.256@gmail.com>
2017-11-18 14:48:48 +0000

    sweet dataframe shit

--- soup.py
+++ soup.py
@@ -1,18 +1,37 @@
 import urllib
 from newspaper import Article
 from bs4 import BeautifulSoup
+import pandas as pd
+
+def parse(pageURL):
+    article=Article(pageURL,language='en')
+    article.download()
+    article.parse()
+    source=pageURL.split('/')[2]
+    
+    pars=article.text.split("\n")
+    
+    i=0
+    firstPar=pars[i]
+    while firstPar.lower().find("image")!=-1 or firstPar=='':
+        i+=1
+        firstPar=pars[i]
+    #save output
+    #order: source site, title, first paragraph
+    return [source,article.title,firstPar]
+
 
 http=urllib3.PoolManager()
-pageURL='http://www.bbc.co.uk/news/42032629'
+#pageURL='http://www.bbc.co.uk/news/42032629'
 #pageURL='http://www.foxnews.com/us/2017/11/18/police-officer-slain-near-pittsburgh-gunman-still-at-large.html'
+#pageURL='http://www.theguardian.com/society/2017/nov/18/lords-push-for-children-to-be-protected-against-tech-giants-by-law'
 response=http.request('GET',pageURL)
-soup = BeautifulSoup(response.data, 'html.parser')
 
-article=Article(pageURL,language='en')
-article.download()
-article.parse()
+data=[['http://www.bbc.co.uk/news/42032629',0],['http://www.foxnews.com/us/2017/11/18/police-officer-slain-near-pittsburgh-gunman-still-at-large.html',0.5],['http://www.theguardian.com/society/2017/nov/18/lords-push-for-children-to-be-protected-against-tech-giants-by-law',0]]
 
-print(article.title)
+df=pd.DataFrame(columns=['source','title','firstPar','fakeness'])
+for i in range(len(data)):
+    src,title,par=parse(data[i][0])
+    df.loc[len(df)]=[src,title,par,data[i][1]]
 
-#print(soup.prettify())
-
+df.to_csv('testdata.csv')
