b79c32156554dc52086e36e096c73e3ee61f4dbd
Jibbles1 <33782437+Jibbles1@users.noreply.github.com>
2017-11-18 16:58:13 +0000

    Add files via upload

--- FakeNews.py
+++ FakeNews.py
@@ -6,11 +6,33 @@ Created on Sat Nov 18 12:09:34 2017
 Author: Julian Hewitt
 
 This is where the code for the Fake News bot goes, probably. Made during BrumHack 7.0 .
-Based on example code by Molly White, thanks Molly!
+Based on example code by Molly White, and Lisa Tagliaferri.
 """
 import tweepy
-from secrets.py import *
+import time
+import json
+from time import sleep
+from secrets import *
 
 auth=tweepy.OAuthHandler(C_KEY,C_SECRET)
 auth.set_access_token(A_TOKEN,A_TOKEN_SECRET)
 api=tweepy.API(auth)
+
+
+ID=api.me()._json['id']
+print(ID)
+#api.update_status('TEST')
+#sleep(100000)
+
+#periodically look for all mentions of myself, anticipate less than N mentions
+#need to make new tweet with text of old tweet inside it, then do a blank retweet.
+#retweeting with own additions doesn't work within Twitter API for some reason.
+#N=1000
+#for tweet in Tweepy.Cursor(api.search,q='@fakenewsbotbrum').items(N):
+ #   tweet=str(tweet)
+  #  raise Exception('exit')
+   # Link= 'Link: ' +''
+   # api.update_status(tweet)
+    
+    #now retweet to get retweet count correct.
+    #api.retweet(tweet['id'])
--- secrets.py
+++ secrets.py
@@ -11,7 +11,7 @@ Eli or Matt is reading this then *someone* forgot to keep this code private.
 
 """
 C_KEY="WUJbjGossZ2gDdbjr5iiajBqT"
-C_SECRET=" MkWozJE3DffWDVLASdiHlB5vS6jtjaRtvJT3OXuBxSk1YML0z1"
-A_TOKEN=" 931865668328345600-A7CXQmjYFnz9SQGwjkzQ6kUwIl1i0fc"
-A_TOKEN_SECRET=" d3IUOi5KIGtnxAIohVPhRf2lINwhbfOf2mXdPDfIpfpLb"
+C_SECRET="MkWozJE3DffWDVLASdiHlB5vS6jtjaRtvJT3OXuBxSk1YML0z1"
+A_TOKEN="931865668328345600-A7CXQmjYFnz9SQGwjkzQ6kUwIl1i0fc"
+A_TOKEN_SECRET="d3IUOi5KIGtnxAIohVPhRf2lINwhbfOf2mXdPDfIpfpLb"
 
