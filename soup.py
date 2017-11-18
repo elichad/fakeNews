0f14d87ee599b42de2f259c2a1727e785ea43358
elichad <eli.chadwick.256@gmail.com>
2017-11-18 17:50:07 +0000

    parsing function can be called from wherever now

--- soup.py
+++ soup.py
@@ -1,6 +1,4 @@
-import urllib
 from newspaper import Article
-from bs4 import BeautifulSoup
 import pandas as pd
 
 def parse(pageURL):
@@ -18,20 +16,17 @@ def parse(pageURL):
         firstPar=pars[i]
     #save output
     #order: source site, title, first paragraph
-    return [source,article.title,firstPar]
+    return [article.title,article.text]
 
-
-http=urllib3.PoolManager()
-#pageURL='http://www.bbc.co.uk/news/42032629'
-#pageURL='http://www.foxnews.com/us/2017/11/18/police-officer-slain-near-pittsburgh-gunman-still-at-large.html'
-#pageURL='http://www.theguardian.com/society/2017/nov/18/lords-push-for-children-to-be-protected-against-tech-giants-by-law'
-response=http.request('GET',pageURL)
-
-data=[['http://www.bbc.co.uk/news/42032629',0],['http://www.foxnews.com/us/2017/11/18/police-officer-slain-near-pittsburgh-gunman-still-at-large.html',0.5],['http://www.theguardian.com/society/2017/nov/18/lords-push-for-children-to-be-protected-against-tech-giants-by-law',0]]
-
-df=pd.DataFrame(columns=['source','title','firstPar','fakeness'])
-for i in range(len(data)):
-    src,title,par=parse(data[i][0])
-    df.loc[len(df)]=[src,title,par,data[i][1]]
-
-df.to_csv('testdata.csv')
+pageURL='http://www.bbc.co.uk/news/uk-england-nottinghamshire-42037305'
+content=parse(pageURL)
+"""
+for i in range(len(urls)):
+    url=urls[i]
+    print(url)
+    src,title,par=parsePage(url)
+    dfout.loc[len(df)]=[title,par,fakenesses[i]]
+    
+print(dfout)
+dfout.to_csv('testData.csv')
+"""
